b88d4118f120678cf54b0843f930f55c86e1097b
Daniel Perez-Rodriguez <112608773+Wemisiu@users.noreply.github.com>
2025-02-01 18:41:47 +0000

    Assignment 2

---
+++ src/MixMatch.java
@@ -0,0 +1,56 @@
+public class MixMatch 
+
+{
+    public static void main(String[] args)
+    
+    
+    {
+        System.out.println();
+        String name = "Daniel";
+        System.out.print(name + " <- String, sequence or string of connected characters");
+        System.out.println();
+        System.out.println();
+
+
+        char letter = 'D';
+        System.out.println(letter + " <- char,  single character or an ASCII value. ");
+        System.out.println();
+        
+        short num1 = 7;
+        System.out.println(num1 + " <- short, An integer between -32,768 and +32,767.");
+        System.out.println();
+
+        int  num2 = 21;
+        System.out.println(num2 + " <- int, An integer between -231 and +231 – 1");
+        System.out.println();
+
+        long num3 = 2004;
+        System.out.println(num3 + " <- long, An integer between -263 and +263 – 1.");
+        System.out.println();
+
+
+        float num4 = 3.4f;
+        System.out.println(num4 + " <- float, A floating-point between ±3.4 * 10-38 and ±3.4 * 1038 and is accurate to 7 decimal places. Must end with an f. Ex: 2.33f ");
+        
+        double num5 = 99.99;
+        System.out.println(num5 + " <- double, A floating-point between ±1.7 * 10-308 and ±1.7 * 10308 and is accurate to 15 decimal places.");
+        System.out.println();
+        
+        boolean var1 = true;
+        System.out.println(var1 + " <- boolean, Can only be true or false.");
+        System.out.println();
+
+
+
+
+        
+
+        
+
+
+      
+
+
+    }
+}
+
